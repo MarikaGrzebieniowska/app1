a48263449b35c2637bf0c42e6757a2a6d7baf77d
Marika Grzebieniowska <marika.grzebieniowska@soonly.pl>
2024-11-24 18:33:29 +0100

    Fixed script

--- app_fixed.py
+++ app_fixed.py
@@ -16,31 +16,33 @@ filename = "model.sv"
 model = pickle.load(open(filename,'rb'))
 # otwieramy wcześniej wytrenowany model
 
+sex_d = {0: "Mężczyzna",2: "Kobieta"}
 pclass_d = {0:"Pierwsza",1:"Druga", 2:"Trzecia"}
 embarked_d = {0:"Cherbourg", 1:"Queenstown", 2:"Southampton"}
 # o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
 
 def main():
 
-	st.set_page_config(page_title="???")
+	st.set_page_config(page_title="Kto przeżyje katastrofę Titanica?")
 	overview = st.container()
 	left, right = st.columns(2)
 	prediction = st.container()
 
-	st.image("https://i.kym-cdn.com/entries/icons/mobile/000/026/489/crying.jpg")
+	st.image("https://en.wikipedia.org/wiki/Sinking_of_the_Titanic#/media/File:St%C3%B6wer_Titanic.jpg")
 
 	with overview:
-		st.title("???")
+		st.title("Kto przeżyje katastrofę Titanica?")
 
 	with left:
 		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
 		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x] )
+		pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
 
 	with right:
-		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=100)
-		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=10)
-		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=10)
-		fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=1)
+		age_slider = st.slider("Wiek", value=1, min_value=0.42, max_value=80.0)
+		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
+		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
+		fare_slider = st.slider("Cena biletu", min_value=0, max_value=512.33, step=0.01)
 
 	data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
 	survival = model.predict(data)
